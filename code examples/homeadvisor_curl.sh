b4ebdb9d728a60fd3f3b322385f9e00e4c9d328a
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2023-12-18 13:10:53 +0200

    Adding code examples/homeadvisor_curl.sh

---
+++ code examples/homeadvisor_curl.sh
@@ -0,0 +1,4 @@
+curl --user user:pass \
+'https://realtime.oxylabs.io/v1/queries' \
+-H "Content-Type: application/json" \
+-d '{"source": "universal", "url": "https://www.homeadvisor.com/task.windows-install-or-replace.46355.html?sequence=0"}'
